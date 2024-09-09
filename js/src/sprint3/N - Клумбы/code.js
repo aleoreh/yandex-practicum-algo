52f1dc6dd7fb5b169d00d1bf6c8f7815a9a744da
Aleksey Orekhov <aleoreh@gmail.com>
2024-09-09 22:26:12 +0300

    N. Клумбы

--- js/src/sprint3/N - Клумбы/code.js
+++ js/src/sprint3/N - Клумбы/code.js
@@ -15,45 +15,85 @@ process.stdin.on('end', run);
 
 // ~~~~~~~~~~~~~~~ решение ~~~~~~~~~~~~~~~ //
 
+/**
+ *
+ * @param {[number, number][]} int1
+ * @param {[number, number][]} int2
+ */
+function isIntersected([x1, x2], [y1, y2]) {
+    return x1 < y2 && y1 <= x2;
+}
+
+/**
+ *
+ * @param {[number, number][]} sections
+ */
+function unionSections(sections) {
+    if (sections.length <= 1) {
+        return sections;
+    }
+
+    const arr = sections.slice().sort(([x0, x1], [y0, y1]) => x0 - y0);
+
+    let acc = [arr[0]];
+    let i = 1;
+
+    while (i < arr.length) {
+        const prev = acc[acc.length - 1];
+        const next = arr[i];
+
+        if (!isIntersected(prev, next)) {
+            acc.push(arr[i]);
+        } else {
+            acc[acc.length - 1] = [
+                Math.min(prev[0], next[0]),
+                Math.max(prev[1], next[1]),
+            ];
+        }
+
+        i++;
+    }
+
+    return acc;
+}
+
+/**
+ *
+ * @param {[number, number][]} input
+ * @returns string[]
+ */
 function solve(input) {
-    return [];
+    const resValue = unionSections(input);
+    return resValue.map(([x, y]) => `${x} ${y}`).join('\n');
+}
+
+function parseInput() {
+    const n = readInt();
+    const values = [];
+    for (let i = 0; i < n; i++) {
+        let line = readArray();
+        values.push([parseInt(line[0]), parseInt(line[1])]);
+    }
+    return values;
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
-function parse() {
-    readInt();
-
-    const values = [];
-    let line = readArray();
-
-    while (line) {
-        values.push([parseInt(line[0]), parseInt(line[1])]);
-        line = readArray();
-    }
-
-    return values;
-}
-
 function run() {
-    const values = parse(_inputLines);
-
+    const values = parseInput();
     const outputArr = solve(values);
-
-    outputArr.forEach((res) => {
-        process.stdout.write(`${res}`);
-    });
+    process.stdout.write(`${outputArr}`);
 }
 
 /**
  * Запускает решение, принимая на вход весь ввод
  * @param {string} inputText - полный текст ввода, не разделённый на строки
  */
+
 function solveUnlined(inputText) {
-    _inputLines = inputText.split('\n');
-
-    const values = parse(_inputLines);
-
+    _curLine = 0;
+    _inputLines = inputText.trim().split('\n');
+    const values = parseInput();
     return solve(values);
 }
 
--- js/src/sprint3/N - Клумбы/code.spec.js
+++ js/src/sprint3/N - Клумбы/code.spec.js
@@ -9,11 +9,39 @@ describe.only('N. Клумбы', () => {
 7 8
 7 8
 2 3
-6 10
-`;
+6 10`;
         const output = `2 3
-6 10
-`;
+6 10`;
+
+        const res = solveUnlined(input);
+
+        expect(res).toEqual(output);
+    });
+
+    it('Проходит фиксированный тест №2', () => {
+        const input = `4
+2 3
+5 6
+3 4
+3 4`;
+        const output = `2 4
+5 6`;
+
+        const res = solveUnlined(input);
+
+        expect(res).toEqual(output);
+    });
+
+    it('Проходит фиксированный тест №3', () => {
+        const input = `6
+1 3
+3 5
+4 6
+5 6
+2 4
+7 10`;
+        const output = `1 6
+7 10`;
 
         const res = solveUnlined(input);
 
